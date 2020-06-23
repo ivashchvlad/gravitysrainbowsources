707414bd1f8af1b71328b8564627be611cf6fa8e
ivashchvlad <ivashchvlad@gmail.com>
2020-06-23 21:25:31 +0400

    scroll to element

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
-    $('.header__burger').click(function () {
-        $(this).toggleClass('active');
-        $('nav').slideToggle();
-        $('body').toggleClass('lock');
+    $('.header__burger').click(menu)
+    $('li > a, h4 > a').click(function() {
+        menu();  
     })
-});
+    $('a').click(function() {
+        let unit = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(unit).offset().top - 50
+        }, 300);
+    })
+})
+
+const menu = function () {
+    $('.header__burger').toggleClass('active')
+    $('nav').slideToggle()
+    $('body').toggleClass('lock')
+}
